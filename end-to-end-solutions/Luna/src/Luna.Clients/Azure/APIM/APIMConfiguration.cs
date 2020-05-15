8bae41d6b2230c95b58c13ef99d3d6c82a0497fd
Junyao Huang <t-junh@microsoft.com>
2020-05-15 11:53:39 +0800

    fix_bugs

--- end-to-end-solutions/Luna/src/Luna.Clients/Azure/APIM/APIMConfiguration.cs
+++ end-to-end-solutions/Luna/src/Luna.Clients/Azure/APIM/APIMConfiguration.cs
@@ -13,8 +13,10 @@ namespace Luna.Clients.Azure.APIM
         public string ResourceGroupname { get; set; }
         public string APIMServiceName { get; set; }
         public string Token { get; set; }
-
         public string APIVersion { get; set; }
         public string VaultName { get; set; }
+        public string UId { get; set; }
+        public string PrimaryKey { get; set; }
+        public string SecondaryKey { get; set; }
     }
 }
--- end-to-end-solutions/Luna/src/Luna.Clients/Azure/APIM/Luna.AI/Auth/APIMAuthHelper.cs
+++ end-to-end-solutions/Luna/src/Luna.Clients/Azure/APIM/Luna.AI/Auth/APIMAuthHelper.cs
@@ -7,17 +7,28 @@ namespace Luna.Clients.Azure.APIM
 {
     public class APIMAuthHelper
     {
-        public static string CreateSharedAccessToken(string primaryKey, string secondaryKey)
+        private string _id;
+        private string _primaryKey;
+        private string _secondaryKey;
+        private DateTime expireTime;
+        public APIMAuthHelper(string id, string primaryKey, string secondaryKey)
         {
-            var id = "integration";
-            var key = string.Format("{0}/{1}", primaryKey, secondaryKey);
-            var expiry = DateTime.UtcNow.AddDays(10);
+            _id = id;
+            _primaryKey = primaryKey;
+            _secondaryKey = secondaryKey;
+            expireTime = DateTime.Now;
+        }
+        public string GetSharedAccessToken()
+        {
+            var key = string.Format("{0}/{1}", _primaryKey, _secondaryKey);
+            if (expireTime.Subtract(DateTime.Now).TotalDays < 1) expireTime = DateTime.UtcNow.AddDays(10);
+            var expiry = expireTime;
             using (var encoder = new HMACSHA512(Encoding.UTF8.GetBytes(key)))
             {
-                var dataToSign = id + "\n" + expiry.ToString("O", CultureInfo.InvariantCulture);
+                var dataToSign = _id + "\n" + expiry.ToString("O", CultureInfo.InvariantCulture);
                 var hash = encoder.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));
                 var signature = Convert.ToBase64String(hash);
-                var encodedToken = string.Format("SharedAccessSignature {0}&{1:o}&{2}", id, expiry, signature);
+                var encodedToken = string.Format("SharedAccessSignature {0}&{1:o}&{2}", _id, expiry, signature);
                 return encodedToken;
             }
         }
